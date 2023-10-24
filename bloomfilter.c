c7f137d235b46464513b6b667b9eb997513910e7
Anirudh <78651548+Anirudh12345678@users.noreply.github.com>
2023-10-24 11:19:57 +0530

    Add files via upload

---
+++ bloomfilter.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#define SIZE 10
+
+int hash(int key);
+bool if_found(int key);
+uint64_t* ptr;
+#define START ptr
+
+int main(int book_no){
+    ptr = (uint64_t*)calloc(SIZE, sizeof(bool));
+    printf("%p\n",ptr);
+    printf("%p\n",*ptr);
+    int x = hash(21);
+    *ptr |= (0x1<<x);
+    printf("%d\n",*ptr);
+    if_found(21);
+    return 1;
+}
+
+int hash(int key){
+    int hash_index = key%SIZE;
+    return hash_index;
+}
+
+bool if_found(int key){
+    int x = hash(key);
+    if(*ptr &= (1<<x)){
+        printf("Found");
+    }else{
+        printf("no");
+    }
+}
